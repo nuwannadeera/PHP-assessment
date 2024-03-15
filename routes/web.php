9b8d48be23f81a54d67c0d03a0ba67427f41d1e0
Nuwan Nadeera <47295198+nuwannadeera@users.noreply.github.com>
2024-03-16 03:03:46 +0530

    end points for customer

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthUserController;
+use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,20 @@ use Illuminate\Support\Facades\Route;
 Route::post('/registration', [AuthUserController::class, 'registrationPost'])->name('registrationPost');
 
 Route::post('/login', [AuthUserController::class, 'loginPost'])->name('loginPost');
+
+
+//--------------------------------------customer routes
+
+Route::post('/addCustomer', [CustomerController::class, 'addCustomer'])
+    ->name('addCustomer');
+
+Route::get('/viewCustomer', [CustomerController::class, 'viewCustomer'])
+    ->name('viewCustomer');
+
+Route::patch('updateCustomerDetail/{id}',[CustomerController::class, 'updateCustomerDetail'])
+    ->name('updateCustomerDetail');
+
+Route::delete('deleteCustomer/{id}',[CustomerController::class, 'deleteCustomer'])
+    ->name('deleteCustomer');
+
+
